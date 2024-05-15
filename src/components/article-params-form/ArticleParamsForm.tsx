96b45d977f0d5fde8180372a0fc0de2a8caa102d
Maria Lachinina <masha.lachinina@mail.ru>
2024-05-15 10:56:59 +0300

    feat: basic functionality

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,19 +2,60 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { ReactNode, SyntheticEvent, useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+export type ArticleParamsFormProps = {
+	children: ReactNode;
+	handleResetClick: () => void;
+	handleFormSubmit: (event: SyntheticEvent) => void;
+};
+
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const rootRef = useRef<HTMLDivElement>(null);
+
+	const articleParamsFormStyle = clsx(styles.container, {
+		[styles.container_open]: isOpen,
+	});
+
+	useEffect(() => {
+		const handleClick = (event: MouseEvent) => {
+			if (!isOpen) return;
+			const { target } = event;
+			if (target instanceof Node && !rootRef.current?.contains(target)) {
+				setIsOpen(false);
+			}
+		};
+
+		window.addEventListener('mousedown', handleClick);
+
+		return () => {
+			window.removeEventListener('mousedown', handleClick);
+		};
+	}, [isOpen]);
+
 	return (
-		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={rootRef}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					setIsOpen((isOpen) => !isOpen);
+				}}
+			/>
+			<aside className={articleParamsFormStyle}>
+				<form className={styles.form} onSubmit={props.handleFormSubmit}>
+					{props.children}
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button
+							title='Сбросить'
+							type='reset'
+							onClick={props.handleResetClick}
+						/>
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,110 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState, SyntheticEvent } from 'react';
 import clsx from 'clsx';
+import styles from './styles/index.module.scss';
+import './styles/index.scss';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { Text } from 'components/text';
+import { Select } from './components/select';
+import { RadioGroup } from './components/radio-group';
+import { Separator } from './components/separator';
 
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from './constants/articleProps';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [fontSize, setFontSize] = useState(defaultArticleState.fontSizeOption);
+	const [fontFamily, setFontFamily] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+	const [fontColor, setFontColor] = useState(defaultArticleState.fontColor);
+	const [bgColor, setBgColor] = useState(defaultArticleState.backgroundColor);
+	const [containerWidth, setContainerWidth] = useState(
+		defaultArticleState.contentWidth
+	);
+
+	const pageStyle = {
+		'--font-family': fontFamily.value,
+		'--font-size': fontSize.value,
+		'--font-color': fontColor.value,
+		'--container-width': containerWidth.value,
+		'--bg-color': bgColor.value,
+	};
+
+	const [style, setStyle] = useState(pageStyle);
+
+	const setDefaultOptions = () => {
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBgColor(defaultArticleState.backgroundColor);
+		setContainerWidth(defaultArticleState.contentWidth);
+	};
+
 	return (
-		<div
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+		<div className={clsx(styles.main)} style={style as CSSProperties}>
+			<ArticleParamsForm
+				handleResetClick={() => {
+					setDefaultOptions();
+					setStyle({
+						'--font-family': defaultArticleState.fontFamilyOption.value,
+						'--font-size': defaultArticleState.fontSizeOption.value,
+						'--font-color': defaultArticleState.fontColor.value,
+						'--container-width': defaultArticleState.contentWidth.value,
+						'--bg-color': defaultArticleState.backgroundColor.value,
+					});
+				}}
+				handleFormSubmit={(e: SyntheticEvent) => {
+					e.preventDefault();
+					setStyle({ ...pageStyle });
+				}}>
+				<Text as={'h2'} size={31} weight={800} uppercase={true}>
+					Задайте параметры
+				</Text>
+				<Select
+					title='Шрифт'
+					selected={fontFamily}
+					options={fontFamilyOptions}
+					onChange={setFontFamily}
+				/>
+				<RadioGroup
+					title='Размер'
+					selected={fontSize}
+					options={fontSizeOptions}
+					name='fontSize'
+					onChange={setFontSize}
+				/>
+				<Select
+					title='Цвет шрифта'
+					selected={fontColor}
+					options={fontColors}
+					onChange={setFontColor}
+				/>
+				<Separator />
+				<Select
+					title='Цвет фона'
+					selected={bgColor}
+					options={backgroundColors}
+					onChange={setBgColor}
+				/>
+				<Select
+					title='Ширина контента'
+					selected={containerWidth}
+					options={contentWidthArr}
+					onChange={setContainerWidth}
+				/>
+			</ArticleParamsForm>
 			<Article />
 		</div>
 	);
